f65f8db7151594e62ee4de3ba764d9031aa65e53
Akash Jadhav <akashjadhav20111995@gmail.com>
2019-06-17 12:10:23 +0530

    commit

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', indexRouter);
 app.use('/api/v1', indexRouter);
 app.use('/api/v1/suggestions', suggestionsRouter);
 
